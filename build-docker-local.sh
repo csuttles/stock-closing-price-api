2363a39ca2122dbdb0246f8e93fe88a2e0b6d766
Chris Suttles <csuttles@users.noreply.github.com>
2022-12-29 22:23:06 -0600

    Update build-docker-local.sh

--- build-docker-local.sh
+++ build-docker-local.sh
@@ -3,6 +3,7 @@ if [[ $(basename $(pwd)) != "stock-closing-price-api" ]];then
     echo "this must be run from the repository root dir"
     exit 1
 fi
+# quick and dirty with my own username; in the real world this should be a service account, point to a registry your company manages, etc.
 DOCKER_USERNAME="csuttles"
 DOCKER_IMAGENAME="stock-closing-price-api"
 DOCKER_VERSION="latest"
